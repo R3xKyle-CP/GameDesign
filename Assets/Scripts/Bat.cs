ff00668e64d182aa35c8ad81b15bf88d4503ee52
Jiaqing Mo <jimo@calpoly.edu>
2018-02-07 18:12:03 -0800

    ememy moving towards player

--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Bat : MonoBehaviour {
 	private Animator anim;
 	float horizontalSpeed;
+	bool isDead = false;
+	// float live = 100;
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
@@ -12,20 +14,37 @@ public class Bat : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-		anim.Play ("Idle");
-		horizontalSpeed = (float)(Mathf.Sin (Time.realtimeSinceStartup) / 30.0);
-		transform.Translate (Vector3.left * horizontalSpeed);
-		//transform.Translate(Vector3.up * Time.deltaTime, Space.World);
-		if (horizontalSpeed < 0f) {
-			if (transform.localScale.x > 0f) {
-				transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-			}
+		//transform.LookAt(Player);
+		/*if (Vector3.Distance (transform.position, Player.position) >= MinDist) {
 
-		}
-		else if (horizontalSpeed > 0f) {
-			if (transform.localScale.x < 0f) {
-				transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+			transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+		}*/
+		anim.Play ("Idle");
+		float distance = Vector3.Distance( Player.Instance.transform.position, transform.position);
+		if (distance > 5) {
+			Vector3 delta = Player.Instance.transform.position - this.transform.position;
+			delta.Normalize ();
+			float moveSpeed = 1 * Time.deltaTime;
+			transform.position = transform.position + (delta * moveSpeed);
+			horizontalSpeed = delta.x;
+
+			//horizontalSpeed = (float)(Mathf.Sin (Time.realtimeSinceStartup) / 30.0);
+			//transform.Translate (Vector3.left * horizontalSpeed);
+			//transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+			if (horizontalSpeed > 0f) {
+				if (transform.localScale.x > 0f) {
+					transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+				}
+
+			} else if (horizontalSpeed < 0f) {
+				if (transform.localScale.x < 0f) {
+					transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+				}
 			}
+		}
+		if (isDead) {
+			anim.Play ("die");
+			Destroy (this);
 		}
 
 	}
--- Assets/Scripts/Cockroach.cs
+++ Assets/Scripts/Cockroach.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 using Prime31;
 public class Cockroach : MonoBehaviour {
 	private Animator anim;
-	private CharacterController2D controller;
+	//private CharacterController2D controller;
 	float horizontalSpeed;
+	bool isDead = false;
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
-		controller = GetComponent<CharacterController2D>();
+		//controller = GetComponent<CharacterController2D>();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (isDead) {
+			anim.Play ("die");
+			Destroy (this);
+		}
 		//if (controller.isGrounded) {
 			horizontalSpeed = (float)(Mathf.Sin (Time.realtimeSinceStartup) / 30.0);
 			transform.Translate (Vector3.left * horizontalSpeed);
