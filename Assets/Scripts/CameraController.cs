2675bfe58d0b0e9759f17fcebcfb3376315d547c
Donovan <donovpdean@gmail.com>
2018-02-21 17:24:54 -0800

    New scene, Audio assets

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -25,7 +25,7 @@ public class CameraController : MonoBehaviour {
         offset = transform.position - player.transform.position;
     }
 
-    void FixedUpdate()
+    void LateUpdate()
     {
         float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
         float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
