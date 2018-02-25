a0efeb38c4ba982baedd908666daf482744327d3
Jiaqing Mo <jimo@calpoly.edu>
2018-02-24 23:22:58 -0800

    player can kill enemies by shooting

--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -6,7 +6,7 @@ public class Bat : MonoBehaviour {
 	private Animator anim;
 	float horizontalSpeed;
 	bool isDead = false;
-	// float live = 100;
+	float live = 100;
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
@@ -19,34 +19,42 @@ public class Bat : MonoBehaviour {
 
 			transform.position += transform.forward * MoveSpeed * Time.deltaTime;
 		}*/
-		anim.Play ("idle");
-		float distance = Vector3.Distance( PlayerController.Instance.transform.position, transform.position);
-		if (distance != 0) {
-			Vector3 delta = PlayerController.Instance.transform.position - this.transform.position;
-			delta.Normalize ();
-			float moveSpeed = 1 * Time.deltaTime;
+		if (live <= 0) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, 0f);
+			anim.Play ("die");
+			//Destroy (this);
+		} else {
+			anim.Play ("idle");
+			float distance = Vector3.Distance (PlayerController.Instance.transform.position, transform.position);
+			if (distance != 0) {
+				Vector3 delta = PlayerController.Instance.transform.position - this.transform.position;
+				delta.Normalize ();
+				float moveSpeed = 1 * Time.deltaTime;
 
-			transform.position = transform.position + (delta * moveSpeed);
-			horizontalSpeed = delta.x;
+				transform.position = transform.position + (delta * moveSpeed);
+				horizontalSpeed = delta.x;
 
-			//horizontalSpeed = (float)(Mathf.Sin (Time.realtimeSinceStartup) / 30.0);
-			//transform.Translate (Vector3.left * horizontalSpeed);
-			//transform.Translate(Vector3.up * Time.deltaTime, Space.World);
-			if (horizontalSpeed > 0f) {
-				if (transform.localScale.x > 0f) {
-					transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-				}
+				//horizontalSpeed = (float)(Mathf.Sin (Time.realtimeSinceStartup) / 30.0);
+				//transform.Translate (Vector3.left * horizontalSpeed);
+				//transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+				if (horizontalSpeed > 0f) {
+					if (transform.localScale.x > 0f) {
+						transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+					}
 
-			} else if (horizontalSpeed < 0f) {
-				if (transform.localScale.x < 0f) {
-					transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+				} else if (horizontalSpeed < 0f) {
+					if (transform.localScale.x < 0f) {
+						transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+					}
 				}
 			}
 		}
-		if (isDead) {
-			anim.Play ("die");
-			Destroy (this);
-		}
 
+
+	}
+
+	public void decreaseLive(){
+		this.live -= 60;
+		return;
 	}
 }
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		//this.gameObject.SetActive (false);
+		if (collision.gameObject.CompareTag ("Bat") ) 
+		{
+			
+			collision.gameObject.GetComponent<Bat> ().decreaseLive ();
+			//this.gameObject.SetActive (false);
+		}
+		if (collision.gameObject.CompareTag ("Cockroach") ) 
+		{
+			
+			collision.gameObject.GetComponent<Cockroach> ().decreaseLive ();
+			//Destroy (this);
+			//this.gameObject.SetActive (false);
+		}
+		if (!collision.gameObject.CompareTag ("Player") ){
+			this.gameObject.SetActive (false);
+		}
+	}
+}
